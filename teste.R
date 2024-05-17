2f102c44e34ea5f7249c16fc190ad6ea5249c219
stephanye rizza <stephanyerizza@gmail.com>
2024-05-17 18:11:20 -0300

    análise 4

--- teste.R
+++ teste.R
@@ -1,4 +1,4 @@
-banco1<-read.csv("banco_final.csv")
+banco1 <-read.csv("banco_final.csv")
 library(tidyverse)
 
 # projeto fantasma - estat#
@@ -92,9 +92,14 @@ ggsave("análise1.pdf", width = 158, height = 93, units = "mm")
 
 dados<-dados%>%rename(Formato=banco1.format)
 
+tentativatab
 
+# 2) análise 2 
 
-# 2) ----
+season.imdb <- table(banco1$season, banco1$imdb)
+season.imdb <-as.data.frame(season.imdb)
+class(banco1$season)
+class(banco1$imdb)
 
 banco1 %>% distinct(banco1$season)
 season<- c(1,2,3,4)
@@ -103,20 +108,45 @@ class(banco1$season)
 banco1$season <- as.character(banco1$season)
 banco1 %>% distinct(season)
 banco1$imdb %>% distinct(imdb)
-bancoteste<- read.csv("banco_final-Copia.csv")
 
-read(banco1)
-banco1$imdb
-banco1$season
-class(banco1$imdb)
-class(banco1$season)
 
 # filtrar season #
+
+season.imdb <- rename(season.imdb, imdb = Var2)
+season.imdb <- rename(season.imdb, temporada = Var1)
+season <- banco1$season 
+imdb <- banco$imdb
+imdb <- as.data.frame(imdb)
+season <- slice(season, -c(153,336,337,339,340,342,357,362,373,375,378,390,392,406,420,421,422,423,433,438,452,453,470,472,484,485,488,490,491,493,514,517,518,524,540,554,555,557,562,573,600,601,51,53,62,335,343,553,556,572,301,308,313,334,338,341,454,471,486,487,489,492,507))
+imdb <- slice(imdb, -c(153,336,337,339,340,342,357,362,373,375,378,390,392,406,420,421,422,423,433,438,452,453,470,472,484,485,488,490,491,493,514,517,518,524,540,554,555,557,562,573,600,601,51,53,62,335,343,553,556,572,301,308,313,334,338,341,454,471,486,487,489,492,507))
+seasonximdb <- data.frame(season, imdb)
+rm(seasonximdb)
+season <- as.data.frame(season)
+season <- rename(season, temporada=V1)
+season1 <- banco$season
+season1 <- as.data.frame(season1)
+season1 <- as.numeric(season1)
+season1 <- which(season1$season1, Season)
+sen <- as.data.frame(season)
+season.imdb <- slice(season.imdb, -c(5, 12, 19, 26, 33,40,47,54,61,68,75,82,89,96,103,110,117,124,131,138,145,152,159,166,173,180,187,194,201,208,215,222,229,236,243,250,257,264,271,278,285,292,299,306,313,320,327,334,341,348,355,362,369, 6, 13,20,27,34,41,48,55,62,69,76,83,90,97,104,111,118,125,132,139,146,153,160,167,174,181,188,195,202,209,216,223,230,237,244,251,258,265,272,279,286,293,300,307,314,321,328,335,342,349,356,363,370, 7, 14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140,147,154,161,168,175,182,189,196,203,210,217,224,231,238,245,252,259,266,273,280,287,294,301,308,315,322,329,336,343,350,357,364,371))
+season.imdb1 <- 
+season.imbd1 <- slice(season.imdb1, c(1,5,9,13,17,21,25,29,33,37,41,45,49,53,57,61,65,69,73,77,81,85,89,93,97,101,105,109,113,117,121,125,129,133,137,141,145,149,153,157,161,165,169,173,177,181,185,189,193,197,201, 205, 209))
+?filter
+ggplot(mpg) +
+  aes(x=factor(""), y= ) +
+  geom_boxplot(fill=c("#A11D21"), width = 0.5) +
+  guides(fill=FALSE) +
+  stat_summary(fun="mean", geom="point", shape=23, size=3, fill="
+white")+
+  labs(x="", y="Consumo em Cidade (milhas/galão)")+
+  theme_estat()
+18
+
 
 banco1 %>%
   filter(season == "1" | season == "2" | season == "3" | season == "4")
 banco1$season
-filtro <- filter(banco1, season %in% c("1","2","3","4"))
+season.imdb <- filter(season.imdb, season %in% c("1","2","3","4"))
 filtro1<- filter(banco1, imdb %in% c(nrow= 51, 53))
 filtro1
 
@@ -124,6 +154,48 @@ tb <- table(banco1$season, banco1$imdb)%>%prop.table(1)
 tb  
 class(filtro1)
 rm(dados2)
+
+ggplot(seasonximdb) +
+  aes(x = season, y = imdb) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
+  ) +
+  labs(x = "Temporada", y = "Nota IMDB") +
+  theme_estat()
+
+ggplot(seasonximdb) +
+  aes(x = reorder(season, imdb), y = imdb) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
+  ) +
+  labs(x = "Temporada", y = "Nota IMDB") +
+  theme_estat()
+
+class(banco1$imbd)
+season.imdb$imdb <- as.numeric(season.imdb$imdb)
+season.imdb$temporada <- as.numeric(season.imdb$temporada)
+
+T1 <- filter(seasonximdb, seasonximdb$season == "1")
+T1
+T2 <- filter(seasonximdb, seasonximdb$season == "2")
+T2
+T3 <- filter(seasonximdb, seasonximdb$season == "3")
+T4<- filter(seasonximdb, seasonximdb$season == "4")
+
+summary(T1) 
+sd(T1$imdb)
+var(T1$imdb)
+summary(T2)
+sd(T2$imdb)
+var(T2$imdb)
+summary(T3)
+sd(T3$imdb)
+var(T3$imdb)
+summary(T4)
+sd(T4$imdb)
+var(T4$imdb)
 
 # análise 3 
 banco1<- banco
@@ -166,7 +238,7 @@ sum(tabela1$frequencia.relativa)
 # gráfico pra terreno mais frequente 
 
 ggplot(data= tabela1, mapping= aes(x = reorder(terreno, frequencia.relativa), y= frequencia.relativa))+
-  geom_col()+
+  geom_col(fill = "#A11D21")+
   labs(x="Tipo de Terreno", y= "Frequência relativa (%)")
            
 # limpando df (tipo de terreno x ativação da armadilha)
@@ -184,4 +256,34 @@ ggplot(data= teste, mapping= aes(x = Var1, y= Freq, fill = ativação ))+
   labs(x="Tipo de terreno", y= "N° de ativações")+
   theme_estat()
 
+# análise 4 ----
 
+imbdxengajamento <- data.frame(banco1$imdb, banco1$engagement)
+ggplot(imbdxengajamento) +
+  aes(x = banco1.imdb, y = banco1.engagement) +
+  geom_point(colour = "#A11D21", size = 3) +
+  labs(
+    x = "Nota IMBD",
+    y = "Engajamento"
+  ) +
+  theme_estat()
+
+
+ggplot(imbdxengajamento) +
+  aes(x = banco1.imdb, y = frequency(imbdxengajamento, banco1.imdb)) +
+  geom_col(colour = "#A11D21", size = 3) +
+  labs(
+    x = "E",
+    y = "f"
+  ) +
+  theme_estat()
+mean(imbdxengajamento$banco1.imdb)
+median(imbdxengajamento$banco1.imdb)
+sd(imbdxengajamento$banco1.imdb)
+
+mean(imbdxengajamento$banco1.engagement)
+median(imbdxengajamento$banco1.engagement)
+sd(imbdxengajamento$banco1.engagement)
+
+imbdxengajamento
+cor.test(imbdxengajamento$banco1.imdb, imbdxengajamento$banco1.engagement, method="pearson")
